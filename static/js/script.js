2434086a0dd4248a4f68965358b1a5317d3253e4
Anatoly Popov <popov.anatoly@gmail.com>
2019-03-02 12:09:24 +0300

    vue django delimiters configured

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $( document ).ready(function() {
     console.log( "jQuery ready!" );
 });
+
+var app = new Vue({
+    el: '#app',
+    delimiters: ['${', '}'],
+    data: {
+        message: 'HELLO VUE'
+    }
+}) 
